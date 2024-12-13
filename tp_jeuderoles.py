9bccfb5da58834358ac8edba6e16bb72467861fc
Cyrille59Delplanque <cyrille.delplanque@labom2iformation.fr>
2024-12-13 12:24:02 +0100

    Correction bug creation objet

--- tp_jeuderoles.py
+++ tp_jeuderoles.py
@@ -46,11 +46,11 @@
 from random import randint
 
 menu1 = "Creer Personnage"
-menu2 = " "
-menu3 = " "
-menu4 = " "
-menu5 = " "
-menu6 = " "
+menu2 = "Voir Inventaire"
+menu3 = "Utliser objet dans Inventaire"
+menu4 = "Voir tous les Personnages et le détail "
+menu5 = "Attaquer Personnage "
+menu6 = "Se deplacer "
 
 def model_personnage()->dict:
     """_model of caracters for the role play_
@@ -79,13 +79,23 @@ def model_objet()->dict:
     }
     
 def visualisation_inventaire(inventaire:list):
-    pass
+    """_inventory visualisation_
+
+    Args:
+        inventaire (list): _list of objet_
+    """
+    if len(inventaire) <0:
+        print("L'Inventaire est vide")
+    else:
+        for objet in inventaire:
+            print (f"{objet["quantité"]} {objet["nom"]}")
 
 def ajout_objet_inventaire(inventaire:list):
     pass
 
 def modif_experience_personnage(personnage:dict):
-    pass
+    personnage["niveau"] += 1
+    personnage["point de vie"] += 20
 
 def utiliser_fonction_soin(personnage:dict):
     pass
@@ -93,20 +103,21 @@ def utiliser_fonction_soin(personnage:dict):
 def supprimer_objet_inventaire(inventaire:list,objet:dict):
     pass
 
-def ajout_point_de_vie(personnage:dict):
-    pass
-
 def afficher_personnage(personnage:dict):
-    pass
+    for denomination,valeur in personnage.items():
+        if denomination != "inventaire":
+            print(f"{denomination} : {valeur}")
+        else:
+            visualisation_inventaire(valeur)
 
 def attaquer(personnage_attaquant:dict,personnage_attaque:dict):
-    pass
+    application_degat(personnage_attaque,10*int(personnage_attaquant["niveau"]))
 
-def application_degat(personnage:dict):
-    pass
+def application_degat(personnage:dict,degat:int):
+    personnage["point de vie"] -= degat
 
-def personnage_est_mort(personnage:dict):
-    pass
+def personnage_est_mort(personnage:dict)->bool:
+    return True if personnage["point de vie"] else False
 
 def recuperation_inventaire(personnage_origine:dict,personnage_destinataire:dict):
     pass
@@ -117,22 +128,22 @@ def menu_du_jeu(choix:int):
             exit()
         case 1:
             print(f"---  {menu1} ---")
-            
+            creation_personnage(model_personnage())
         case 2:
             print(f"---  {menu2} ---")
-            
+            #voir inventaire
         case 3:
             print(f"---  {menu3} ---")
-            
+            #utiliser objet ds inventaire
         case 4:
             print(f"---  {menu4} ---")
-            
+            #voir tout les personnages et le detail
         case 5:
             print(f"---  {menu5} ---")
-            
+            #attaquer personnage
         case 6:
             print(f"---  {menu6} ---")
-            
+            #se deplacer
         case _:
             print("Selection Incorrecte")
     print() 
@@ -178,7 +189,7 @@ def creation_objet(objet:dict,nom_objet:str=None,quantite:int=0)->dict:
     Returns:
         dict: _the object created_
     """
-    if nom_objet is not None:
+    if nom_objet is None:
         for cle in objet.keys():
             objet[cle]=controle_saisie(cle)
     else:
@@ -245,9 +256,24 @@ def ajout_personnage_jeu(liste_personnage:list, personnage:dict):
     liste_personnage.append(personnage)
 
 def visualisation_liste_personnage(liste_personnage:list):
-    pass
+    """_show the list of caracter in game_
+
+    Args:
+        liste_personnage (list): _list of caracter_
+    """
+    for personnage in liste_personnage:
+        print (f"{personnage["nom"]} {personnage["classe"]}") 
 
 def controle_objet_dans_la_liste(liste_objet:list,objet:dict)->tuple:
+    """_control if the objet name is already in the list of object_
+
+    Args:
+        liste_objet (list): _list of object_
+        objet (dict): _object_
+
+    Returns:
+        tuple: _bool,index bool True if in the list, False if not and index is the index in the list if is found_
+    """
     if len(liste_objet) <0:
         return False,_
     index_liste_inventaire = 0
@@ -258,7 +284,19 @@ def controle_objet_dans_la_liste(liste_objet:list,objet:dict)->tuple:
     return False,_
 
 def ajout_quantite_objet_dans_la_liste(liste_objet:list,index_list:int,quantite:int):
+    """_add quanity of an objet alreay in the list_
+
+    Args:
+        liste_objet (list): _list of object_
+        index_list (int): _position of the object in the list_
+        quantite (int): _quantity to add_
+    """
     liste_objet[index_list]["quantité"] += quantite
 
 #définir max de point de vie, max de niveau (l'inclure dans le controle de la saisie), definir fonction deplacement pour trouver des objets
 #définir qté max d'objet dans l'inventaire et controler en cas d'ajout
+#creer un sous menu inventaire pour utiliser les objets
+
+#test = creation_objet(model_objet())
+test2= creation_objet(model_objet(),"Potion de soin",3)
+print(test2)
